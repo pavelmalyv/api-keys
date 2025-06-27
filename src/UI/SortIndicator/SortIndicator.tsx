f3591bf3bacd1f32d604feed8ae8e28f4f156ed2
Pavel <pavelmalyv@gmail.com>
2025-06-27 19:17:48 +0300

    feat(ui): добавить компонент SortIndicator

---
+++ src/UI/SortIndicator/SortIndicator.tsx
@@ -0,0 +1,23 @@
+import type { SortOrder, Override } from '@/types';
+
+import Box, { type BoxProps } from '@mui/material/Box';
+import { visuallyHidden } from '@mui/utils';
+
+type SortIndicatorOwnProps = {
+	order: SortOrder;
+};
+
+type SortIndicatorProps = Override<BoxProps, SortIndicatorOwnProps>;
+
+const SortIndicator = ({ order, children, ...props }: SortIndicatorProps) => {
+	return (
+		<Box component="span" sx={visuallyHidden} {...props}>
+			{order === 'desc' ? 'отсортировано по убыванию' : 'отсортировано по возрастанию'}
+
+			{children}
+		</Box>
+	);
+};
+
+export default SortIndicator;
+export type { SortIndicatorProps };
