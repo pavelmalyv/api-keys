802dd1fe90c209b5598f41081d3e402910b4e594
Pavel <pavelmalyv@gmail.com>
2025-06-27 19:17:48 +0300

    feat(store): подключить slice модуля api-keys к store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { apiKeysReducer } from '@modules/ApiKeys';
 
 export const store = configureStore({
-	reducer: {},
+	reducer: {
+		apiKeys: apiKeysReducer,
+	},
 });
 
 export type RootState = ReturnType<typeof store.getState>;
