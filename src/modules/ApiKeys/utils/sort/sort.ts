465ac5b3c7b806c4b736fb2d71be4578b26cc59a
Pavel <pavelmalyv@gmail.com>
2025-06-27 19:17:48 +0300

    feat(apiKeys): добавить утилиты sort

---
+++ src/modules/ApiKeys/utils/sort/sort.ts
@@ -0,0 +1,31 @@
+import type { SortOrder, SortType } from '@/types';
+
+const assertNever = (x: never): never => {
+	throw new Error(`Unexpected value: ${x}`);
+};
+
+export const combineSort = (
+	a: string | number | boolean,
+	b: string | number | boolean,
+	order: SortOrder,
+	type: SortType,
+) => {
+	let result = 0;
+
+	switch (type) {
+		case 'string': {
+			result = String(a).localeCompare(String(b));
+			break;
+		}
+		case 'number':
+		case 'date': {
+			result = Number(a) - Number(b);
+			break;
+		}
+		default: {
+			return assertNever(type);
+		}
+	}
+
+	return order === 'asc' ? result : -result;
+};
