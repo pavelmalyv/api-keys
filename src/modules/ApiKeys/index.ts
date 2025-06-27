b02654b41ea7c7ead24d8da94389911448112f55
Pavel <pavelmalyv@gmail.com>
2025-06-27 19:17:48 +0300

    feat(apiKeys): добавить redux-slice модуля

---
+++ src/modules/ApiKeys/index.ts
@@ -0,0 +1,1 @@
+export { default as apiKeysReducer } from './store/apiKeysSlice';
---
+++ src/modules/ApiKeys/store/apiKeysSlice.ts
@@ -0,0 +1,38 @@
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+import type { ApiKey, ApiKeysState } from '../types';
+
+type AddApiKeyOptions = Omit<ApiKey, 'isActive'> & Partial<Pick<ApiKey, 'isActive'>>;
+type UpdateApiKeyOptions = Partial<Omit<ApiKey, 'id'>> & Pick<ApiKey, 'id'>;
+
+const initialState: ApiKeysState = {};
+
+export const apiKeysSlice = createSlice({
+	name: 'apiKeys',
+	initialState,
+	reducers: {
+		addApiKey: (store, action: PayloadAction<AddApiKeyOptions>) => {
+			const { id, isActive = true, ...options } = action.payload;
+			store[id] = {
+				id,
+				...options,
+				isActive,
+			};
+		},
+		updateApiKey: (store, action: PayloadAction<UpdateApiKeyOptions>) => {
+			const { id, ...options } = action.payload;
+			store[id] = {
+				...store[id],
+				...options,
+			};
+		},
+		deleteApiKeys: (store, action: PayloadAction<ApiKey['id'][]>) => {
+			for (const id of action.payload) {
+				delete store[id];
+			}
+		},
+	},
+});
+
+export const { addApiKey, updateApiKey, deleteApiKeys } = apiKeysSlice.actions;
+export default apiKeysSlice.reducer;
+export type { ApiKeysState };
