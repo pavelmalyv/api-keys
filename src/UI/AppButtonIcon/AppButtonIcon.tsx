8a39d2a75aed27ab7ff7c08787c349cb1b92861f
Pavel <pavelmalyv@gmail.com>
2025-06-27 19:17:48 +0300

    feat(ui): добавить компонент AppButtonIcon

---
+++ src/UI/AppButtonIcon/AppButtonIcon.tsx
@@ -0,0 +1,15 @@
+import type { IconButtonProps } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+
+type AppButtonIconProps = IconButtonProps;
+
+const AppButtonIcon = ({ size = 'medium', children, ...props }: AppButtonIconProps) => {
+	return (
+		<IconButton size={size} {...props}>
+			{children}
+		</IconButton>
+	);
+};
+
+export default AppButtonIcon;
+export type { AppButtonIconProps };
